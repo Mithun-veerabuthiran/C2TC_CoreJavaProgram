4a0cb41d0eea283bc116392875b823e22237c73e
programminds80@gmail.com <142045435+HackVenturer@users.noreply.github.com>
2025-07-21 14:23:44 +0530

    the demo 2 with qp can we overload the main method = yes ; in above program which main method will run : Method Signature with the main with string[] args will run

--- C2TC_CoreJavaProgram/src/day1/basics/SampleDemo.java
+++ C2TC_CoreJavaProgram/src/day1/basics/SampleDemo.java
@@ -1,11 +1,18 @@
 package day1.basics;
 
 public class SampleDemo {
+	// declare of the static block 
+	static {
+		System.out.println("Welcome from static"+"static block 2");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to JFS...");
 		System.out.println("next Line");
 		}
+	static {
+		System.out.println("Welcome from static"+"static block 1");
+	}
 	
 
 }
---
+++ C2TC_CoreJavaProgram/src/day1/basics/sample2Demo.java
@@ -0,0 +1,10 @@
+package day1.basics;
+
+public class sample2Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
