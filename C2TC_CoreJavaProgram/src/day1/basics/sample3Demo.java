05529e834b1b1b80195d6b1820ae01ad6eacaa0d
programminds80@gmail.com <142045435+HackVenturer@users.noreply.github.com>
2025-07-21 14:37:41 +0530

    argument passing parameter  code

---
+++ C2TC_CoreJavaProgram/src/day1/basics/sample3Demo.java
@@ -0,0 +1,18 @@
+package day1.basics;
+
+public class sample3Demo {
+
+	public static void main(String[] args) {
+		// this program with args we are getting the input with help of wrapper class we are 
+		// convert the string into the integer with args parameter then add of the parameter
+		// display the output
+		// TODO Auto-generated method stub
+		int a,b,c;
+		a =Integer.parseInt(args[0]);
+		b =Integer.parseInt(args[1]);
+		c=a+b;
+		System.out.println("Addition of a ="+a+" and b ="+b+" is "+c);
+
+	}
+
+}
