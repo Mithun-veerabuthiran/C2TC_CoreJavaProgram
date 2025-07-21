613d6a5e5125ac5b25ebae5143c11a924efc0e83
programminds80@gmail.com <142045435+HackVenturer@users.noreply.github.com>
2025-07-21 14:24:42 +0530

    save it

--- C2TC_CoreJavaProgram/src/day1/basics/SampleDemo.java
+++ C2TC_CoreJavaProgram/src/day1/basics/SampleDemo.java
@@ -1,7 +1,9 @@
 package day1.basics;
 
 public class SampleDemo {
-	// declare of the static block 
+	// in this which static block will run first and give the output of it :
+	//block 2 , block1 ,main block this way it will work
+    // declare of the static block 
 	static {
 		System.out.println("Welcome from static"+"static block 2");
 	}
--- C2TC_CoreJavaProgram/src/day1/basics/sample2Demo.java
+++ C2TC_CoreJavaProgram/src/day1/basics/sample2Demo.java
@@ -2,9 +2,19 @@ package day1.basics;
 
 public class sample2Demo {
 
+	  public static void main() {
+		System.out.println("welcome from main 1");
+	}
+	  public static void main(char c[] ) {
+		  System.out.println("welcome from main 2");
+	}
+	  public static void main(String ar) {
+		System.out.println("welcome from main 3");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to JFS...");
+		System.out.println("next Line");
 	}
 
 }
