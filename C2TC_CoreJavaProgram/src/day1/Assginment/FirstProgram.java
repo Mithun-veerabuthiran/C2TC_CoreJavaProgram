15979e24059b5f15af1212af27b9f622fa3a2ea4
programminds80@gmail.com <142045435+HackVenturer@users.noreply.github.com>
2025-07-21 23:38:41 +0530

    The day 1 assignment is complete.

---
+++ C2TC_CoreJavaProgram/src/day1/Assginment/FirstProgram.java
@@ -0,0 +1,10 @@
+package day1.Assginment;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World ,sir");
+	}
+
+}
