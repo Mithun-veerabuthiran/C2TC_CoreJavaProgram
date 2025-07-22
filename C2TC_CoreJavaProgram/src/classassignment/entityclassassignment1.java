a906c1581a0a6bd3ae26aff380aaaa004cd1eecb
programminds80@gmail.com <142045435+HackVenturer@users.noreply.github.com>
2025-07-22 13:24:47 +0530

    The class assignment is complete

---
+++ C2TC_CoreJavaProgram/src/classassignment/entityclassassignment1.java
@@ -0,0 +1,13 @@
+package classassignment;
+
+public class entityclassassignment1 {
+
+	public static void main(String[] args) {
+	//1.Write a java program Student class and collect 3 students details in main program.
+	//	(Package name: entityclassassignment1)
+
+		
+
+	}
+
+}
