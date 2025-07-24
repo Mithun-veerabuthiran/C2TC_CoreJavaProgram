45f8eeb336f61004f2c1a361cdc5bcf836383f34
programminds80@gmail.com <142045435+HackVenturer@users.noreply.github.com>
2025-07-24 14:19:34 +0530

    Working on the ArrayList

---
+++ C2TC_CoreJavaProgram/src/day1/pra/ArrayListImpro.java
@@ -0,0 +1,13 @@
+package day1.pra;
+
+import java.util.ArrayList;
+
+public class ArrayListImpro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        ArrayList<String> list = new ArrayList<>();
+        
+	}
+
+}
