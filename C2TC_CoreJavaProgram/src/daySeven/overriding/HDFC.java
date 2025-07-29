ef704bbb681ccc275678d1dbecdabcb728765507
programminds80@gmail.com <142045435+HackVenturer@users.noreply.github.com>
2025-07-29 10:36:12 +0530

    Overriding is complete

---
+++ C2TC_CoreJavaProgram/src/daySeven/overriding/HDFC.java
@@ -0,0 +1,10 @@
+package daySeven.overriding;
+
+//subclass
+public class HDFC extends RBI {
+	@Override
+	public float getRateOfInterest() {
+		return 6.8f;
+	}
+
+}
---
+++ C2TC_CoreJavaProgram/src/daySeven/overriding/ICICI.java
@@ -0,0 +1,11 @@
+package daySeven.overriding;
+
+
+//subclass
+public class ICICI extends RBI {
+	@Override
+	public float getRateOfInterest() {
+		return 6.9f;
+	}
+
+}
